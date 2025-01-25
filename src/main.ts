cdedff89e2acbd6d64203cea78e0f4c942c55504
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-25 12:44:30 +0100

    remove commented-out bootstrapApplication code with providers

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+/* bootstrapApplication(AppComponent, {
+  providers: [TasksService],
+}).catch((err) => console.error(err)); */
+
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
