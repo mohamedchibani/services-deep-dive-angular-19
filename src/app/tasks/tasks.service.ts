2d741fc2883005119f402da60bd6f2fae1532b7a
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-25 12:17:09 +0100

    refactor tasks-list component to use tasks service for task retrieval and improve template syntax

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -5,7 +5,9 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class TasksService {
-  tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+
+  allTasks = this.tasks.asReadonly();
 
   addTask(tasksData: { title: string; description: string }) {
     const newTask: Task = {
